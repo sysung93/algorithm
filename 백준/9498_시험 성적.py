1299db4b0b75ea485c52e924f6831ff056e29791
sysung93 <sysung93@gmail.com>
2021-10-03 23:51:36 +0900

    시험성적, 두수비교하기,나머지

---
+++ 백준/9498_시험 성적.py
@@ -0,0 +1,13 @@
+score = input()
+score = int(score)
+
+if score >= 90:
+    print("A")
+elif score >= 80:
+    pirnt("B")
+elif score >= 70:
+    print("C")
+elif score >= 60:
+    print("D")
+else:
+    print("F")
