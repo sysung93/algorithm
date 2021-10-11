8cecb8912d2915ea0e95aac808ac7cc61938e4b1
sysung93 <sysung93@gmail.com>
2021-10-11 22:42:18 +0900

    Day1

---
+++ java/src/Day1.java
@@ -0,0 +1,17 @@
+/*
+작성자 : 성소영
+작성일 : 2021.10.11
+ */
+
+public class Day1  {
+    public static void main(String[] args) {
+        System.out.println("Hello, welcome to the java world!");
+
+        int x = 1;
+        int y = 2;
+
+        int result = x + y;
+        System.out.println(x + "+" + y + "=" + result);
+
+    } // end of main
+} //end of class
