5308876afc095177d8b5693a8a0758a76e26091d
sysung93 <sysung93@gmail.com>
2021-10-13 23:01:00 +0900

    Day2변수와타입

---
+++ java/src/Day2_변수와타입.java
@@ -0,0 +1,18 @@
+public class Day2_변수와타입 {
+    public static void main(String[] args) {
+         //변수값읽기
+        int value = 10;
+        int result = value + 10;
+        System.out.println(result);
+
+        //boolean 타입
+        boolean stop = true;
+        if(stop){
+            System.out.println("중지합니다");
+        } else{
+            System.out.println("시작합니다");
+        }
+    }
+
+}
+ 
