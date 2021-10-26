4fc43128c623828f04444baea54041339341612b
sysung93 <sysung93@gmail.com>
2021-10-27 00:44:22 +0900

    A+B-4

--- java/src/B_10951_AB4.java
+++ java/src/B_10951_AB4.java
@@ -1,26 +1,29 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class B_10951_AB4 {
     public static void main(String[] args) throws IOException {
 
-        Scanner sc = new Scanner(System.in);
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         StringTokenizer st;
+        String str;
 
-        while (sc.hasNext()){
-            st = new StringTokenizer(br.readLine()," ");
-
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            System.out.println(a+b);
+        while(true){
+            String line = br.readLine();
+            if (line == "" || line == null){
+                break;
+            }else{
+                st = new StringTokenizer(line);
+                int a = Integer.parseInt(st.nextToken());
+                int b = Integer.parseInt(st.nextToken());
+                sb.append(a+b).append("\n");
+            }
         }
+
+        System.out.print(sb);
+
     }
-
-
 }
-
